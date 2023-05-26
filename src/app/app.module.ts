7e9fdd5c6a0a419b073f103cf90294d3fb0da4b8
FrahOrru <francesca.orru00@outlook.it>
2023-05-26 10:13:49 +0200

    fix hierarcy and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ProductGridComponent } from './components/product-grid/product-grid.component';
-import { ProductCartComponent } from './components/product-cart/product-cart.component';
-import { CartTotalComponent } from './components/cart-total/cart-total.component';
+import { ProductCardComponent } from './components/products/product-grid/product-card/product-card.component';
+import { ProductGridComponent } from './components/products/product-grid/product-grid.component';
+import { ProductCartComponent } from './components/products/cart-total/product-cart/product-cart.component';
+import { CartTotalComponent } from './components/products/cart-total/cart-total.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
